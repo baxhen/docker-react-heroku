dc7de79cd099401dc046b1acb1e3d652a138ef9b
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-26 11:01:02 -0300

    chenged app text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there</p>
+        <p>Deployed with at Heroku with Docker and Travis CI</p>
         <a
           className="App-link"
           href="https://reactjs.org"
